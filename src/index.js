c2c2e127a49aac0706208dbc838e5720226b8b5f
Alexander Gudulin <alexandr.gudulin@gmail.com>
2016-11-13 20:31:25 +0100

    add simple conditions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 class Game {
+  constructor () {
+    this.total = 0
+  }
+
   score () {
-    return 0
+    return this.total
+  }
+
+  roll (pins) {
+    if (pins < 0) {
+      throw new Error('Can\'t roll less than 0')
+    } else if (pins > 10) {
+      throw new Error('Can\'t roll more than 10')
+    }
+
+    this.total += pins
   }
 }
 
--- src/index.spec.js
+++ src/index.spec.js
@@ -11,4 +11,12 @@ describe('Bowling game', () => {
   it('should have score: 0 at the beginning', () => {
     expect(game.score()).toEqual(0)
   })
+
+  it('should not roll less than 0 pins', () => {
+    expect(() => game.roll(-1)).toThrow(new Error('Can\'t roll less than 0'))
+  })
+
+  it('should not roll more than 10 pins', () => {
+    expect(() => game.roll(11)).toThrow(new Error('Can\'t roll more than 10'))
+  })
 })
